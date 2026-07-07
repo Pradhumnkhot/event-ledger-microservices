d1796e61e3d0854e06b4a7cefe06a1430e24e0e0
Pradhumnkhot <103945184+Pradhumnkhot@users.noreply.github.com>
2026-07-08 00:34:34 +0530

    Update Event.java

--- eventLedger/src/main/java/com/traceledger/gateway/model/Event.java
+++ eventLedger/src/main/java/com/traceledger/gateway/model/Event.java
@@ -5,20 +5,53 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Event {
+
     @Id
+    private String eventId;
+
     private String accountId;
+
     private double balance;
 
-    public Event() {}
-    public Event(String accountId, double balance) {
+    public Event() {
+    }
+
+    public Event(String eventId, String accountId, double balance) {
+        this.eventId = eventId;
         this.accountId = accountId;
         this.balance = balance;
     }
 
-    // Getters and setters
-    public String getAccountId() { return accountId; }
-    public void setAccountId(String accountId) { this.accountId = accountId; }
+    public String getEventId() {
+        return eventId;
+    }
 
-    public double getBalance() { return balance; }
-    public void setBalance(double balance) { this.balance = balance; }
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "eventId='" + eventId + '\'' +
+                ", accountId='" + accountId + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
 }
