15cf85d0a9fd85db962b6304d732696f67f444d9
Pradhumnkhot <103945184+Pradhumnkhot@users.noreply.github.com>
2026-07-08 00:40:35 +0530

    Update Transaction.java

--- accounthandle/src/main/java/com/traceledger/account/model/Transaction.java
+++ accounthandle/src/main/java/com/traceledger/account/model/Transaction.java
@@ -1,40 +1,61 @@
 package com.traceledger.account.model;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import java.time.LocalDateTime;
 
 @Entity
 public class Transaction {
+
     @Id
     private String eventId;
-    private String accountId;
-    private double amount;
-    private LocalDateTime timestamp;
-	public String getEventId() {
-		return eventId;
-	}
-	public void setEventId(String eventId) {
-		this.eventId = eventId;
-	}
-	public String getAccountId() {
-		return accountId;
-	}
-	public void setAccountId(String accountId) {
-		this.accountId = accountId;
-	}
-	public double getAmount() {
-		return amount;
-	}
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
-	public LocalDateTime getTimestamp() {
-		return timestamp;
-	}
-	public void setTimestamp(LocalDateTime timestamp) {
-		this.timestamp = timestamp;
-	}
 
-    
+    private String accountId;
+
+    private double amount;
+
+    private LocalDateTime timestamp;
+
+    public Transaction() {
     }
+
+    public Transaction(String eventId, String accountId, double amount, LocalDateTime timestamp) {
+        this.eventId = eventId;
+        this.accountId = accountId;
+        this.amount = amount;
+        this.timestamp = timestamp;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
